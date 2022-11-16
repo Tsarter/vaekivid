f57ba3c43eaca33c4efc954e6b9831ff5ea0e05e
Allan Lip <alllip@ttu.ee>
2022-11-16 23:52:56 +0200

    Revert "section of landing page, vali spetsialistide kivi"
    This reverts commit d6c227bf6af896656b5ad427a4d9f40eae5c0e07.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import Card from "./components/Card";
-import kjall from "./assets/kjall.png"
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Või vali mõni spetsialistide poolt loodud väekivi</h1>
-      <div className="card-table">
-        <Card name="Kjallfrogenäider" price="21.99€" image={kjall}/>
-        <Card name="Kjallfrogenäider" price="21.99€" image={kjall}/>
-        <Card name="Kjallfrogenäider" price="21.99€" image={kjall}/>
-        <Card name="Kjallfrogenäider" price="21.99€" image={kjall}/>
-        <Card name="Kjallfrogenäider" price="21.99€" image={kjall}/>
-        <Card name="Kjallfrogenäider" price="21.99€" image={kjall}/>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
