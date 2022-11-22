c14b82510487586c5b608a817d57a86f2f0cf831
tatall <tatall@taltech.ee>
2022-11-22 22:50:46 +0200

    added header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import AboutPage from "./pages/AboutPage";
 import BlogPage from "./pages/BlogPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import HomePage from "./pages/HomePage";
+import Layout from "./components/layout/Layout";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/meist" element={<AboutPage />} />
-      <Route path="/blogi" element={<BlogPage />} />
-      {/* <Route path="/pood" element={<FavoritesPage />} /> */}
-      <Route path="/ostukorv" element={<CheckoutPage />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/meist" element={<AboutPage />} />
+        <Route path="/blogi" element={<BlogPage />} />
+        <Route path="/ostukorv" element={<CheckoutPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
