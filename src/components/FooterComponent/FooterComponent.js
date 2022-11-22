f3ef13877d4914745a6e459992b919f584cb5080
Allan Lip <alllip@ttu.ee>
2022-11-22 21:01:31 +0200

    Added fonts, created FooterComponent

---
+++ src/components/FooterComponent/FooterComponent.js
@@ -0,0 +1,30 @@
+import logo from "../../logo.svg";
+import "./FooterComponent.css";
+
+function FooterComponent() {
+
+
+    return (
+        <footer className="footerContainer">
+            <div className="hrContainer rowFlex">
+                <hr/>
+                <hr/>
+            </div>
+            <div className="imgContainer rowFlex">
+                <img alt="company-logo" src={logo}/>
+            </div>
+            <div className="buttonContainer rowFlex">
+                <div className="text">LANGUAGES</div>
+                <div className="text">PRIVACY POLICY</div>
+                <div className="text">LEGAL TERMS</div>
+            </div>
+            <div className="rowFlex">
+                <div className="text">© 2022 | VÄEKIVID</div>
+            </div>
+        </footer>
+    );
+}
+
+
+
+export default FooterComponent;
