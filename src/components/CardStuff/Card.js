fd7814949720255699fb24b2453016a803f13ac5
tatall <tatall@taltech.ee>
2022-11-23 18:47:47 +0200

    looks ugly but the rocks are there

---
+++ src/components/CardStuff/Card.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+import classes from "./Card.module.css";
+
+function Card(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
+  return (
+    <div className={classes.card}>
+      <div className={classes.image_div}>
+        <img className={classes.rock_image} src={props.image}></img>
+      </div>
+      <h2 className={classes.name_text}>{props.name}</h2>
+      <h2 className={classes.price_text}>{props.price}</h2>
+      <div className={classes.actions}>
+        <button className={classes.btn} onClick={deleteHandler}>
+          Lisa korvi
+        </button>
+      </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
+    </div>
+  );
+}
+
+export default Card;
