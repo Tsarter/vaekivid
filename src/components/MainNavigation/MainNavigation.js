fc2020af30d94bfef0d0a07084ccf9e1943c0960
tatall <tatall@taltech.ee>
2022-11-23 18:54:16 +0200

    tiny ui changes to navigation

--- src/components/MainNavigation/MainNavigation.js
+++ src/components/MainNavigation/MainNavigation.js
@@ -7,41 +7,39 @@ import cart from "../../assets/cart.svg";
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <nav>
-        <div className={classes.pages}>
-          <div>
-            <Link to="/">
-              <h4>Kodu</h4>
-            </Link>
-          </div>
-          <div>
-            <Link to="/meist">
-              <h4>Meist</h4>
-            </Link>
-          </div>
-          <div>
-            <Link to="/blogi">
-              <h4>Blogi</h4>
-            </Link>
-          </div>
-          <div>
-            <Link to="/">
-              <h4>Pood</h4>
-            </Link>
-          </div>
-        </div>
-        <div className={classes.logo}>
-          <div className={classes.logo_text}>Väekivid</div>
-          <img src={logo} alt="Väekivid logo"></img>
-        </div>
-        <div className={classes.icons}>
-          <img src={search} alt="otsing"></img>
-          <img src={user} alt="kasutaja"></img>
-          <Link to="/ostukorv">
-            <img src={cart} alt="ostukorv"></img>
+      <div className={classes.pages}>
+        <div>
+          <Link to="/">
+            <h4>Kodu</h4>
           </Link>
         </div>
-      </nav>
+        <div>
+          <Link to="/meist">
+            <h4>Meist</h4>
+          </Link>
+        </div>
+        <div>
+          <Link to="/blogi">
+            <h4>Blogi</h4>
+          </Link>
+        </div>
+        <div>
+          <Link to="/">
+            <h4>Pood</h4>
+          </Link>
+        </div>
+      </div>
+      <div className={classes.logo}>
+        <div className={classes.logo_text}>Väekivid</div>
+        <img src={logo} alt="Väekivid logo"></img>
+      </div>
+      <div className={classes.icons}>
+        <img src={search} alt="otsing"></img>
+        <img src={user} alt="kasutaja"></img>
+        <Link to="/ostukorv">
+          <img src={cart} alt="ostukorv"></img>
+        </Link>
+      </div>
     </header>
   );
 }
