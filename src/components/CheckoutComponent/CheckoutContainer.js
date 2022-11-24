cd57a5c57e1968bccb97691b21bb64a36997c2d7
Allan Lip <alllip@ttu.ee>
2022-11-24 21:56:12 +0200

    Added "Vali tarneviis" title to CheckoutComp

--- src/components/CheckoutComponent/CheckoutContainer.js
+++ src/components/CheckoutComponent/CheckoutContainer.js
@@ -10,6 +10,7 @@ import truck from "../../assets/delivery-truck.svg";
 function CheckoutContainer() {
     return (
         <div className="container">
+            <h1 className="containerTitle">Vali tarneviis</h1>
             <DeliveryMethodComponent title="Pakiautomaat">
                 <CourierOptionComponent title="Omniva" img={omniva} alt="Omniva Logo" price="2,19 €"/>
                 <CourierOptionComponent title="DPD" img={dpd} alt="DPD Logo" price="2,99 €"/>
