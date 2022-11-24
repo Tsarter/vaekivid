72021dcb56dd0e52ad74129109ca231f1e4488ee
Allan Lip <alllip@ttu.ee>
2022-11-25 01:06:19 +0200

    Added UI change on selection, made it so only one courier can be selected at a time.

--- src/components/CheckoutComponent/CheckoutContainer.js
+++ src/components/CheckoutComponent/CheckoutContainer.js
@@ -5,23 +5,32 @@ import dpd from "../../assets/dpd.svg";
 import omniva from "../../assets/omniva.svg";
 import catapult from "../../assets/catapult.svg";
 import building from "../../assets/building.svg";
-import truck from "../../assets/delivery-truck.svg";
+import React from 'react';
+import {useState} from 'react';
 
 function CheckoutContainer() {
+    const [selected, useSelected] = useState();
+    const handleSelected = {
+        selected: selected,
+        useSelected: (e) => useSelected(e)
+    }
+
+
     return (
+
         <div className="container">
             <h1 className="containerTitle">Vali tarneviis</h1>
             <DeliveryMethodComponent title="Pakiautomaat">
-                <CourierOptionComponent title="Omniva" img={omniva} alt="Omniva Logo" price="2,19 €" link=""/>
-                <CourierOptionComponent title="DPD" img={dpd} alt="DPD Logo" price="2,99 €" link=""/>
+                <CourierOptionComponent handleSelected={handleSelected} title="Omniva" img={omniva} alt="Omniva Logo" price="2,19 €"/>
+                <CourierOptionComponent handleSelected={handleSelected} title="DPD" img={dpd} alt="DPD Logo" price="2,99 €"/>
             </DeliveryMethodComponent>
             <DeliveryMethodComponent title="Kuller">
-                <CourierOptionComponent title="Omniva" img={omniva} alt="Omniva Logo" price="4,29 €" link=""/>
-                <CourierOptionComponent title="DPD" img={dpd} alt="DPD Logo" price="4,99 €" link=""/>
+                <CourierOptionComponent handleSelected={handleSelected} title="Omniva" img={omniva} alt="Omniva Logo" price="4,29 €"/>
+                <CourierOptionComponent handleSelected={handleSelected} title="DPD" img={dpd} alt="DPD Logo" price="4,99 €"/>
             </DeliveryMethodComponent>
             <DeliveryMethodComponent title="Muu">
-                <CourierOptionComponent title="Katapult" img={catapult} alt="Catapult" price="Tasuta" link=""/>
-                <CourierOptionComponent title="Ise järgi" img={building} alt="Building" price="Tasuta" link=""/>
+                <CourierOptionComponent handleSelected={handleSelected} title="Katapult" img={catapult} alt="Catapult" price="Tasuta"/>
+                <CourierOptionComponent handleSelected={handleSelected} title="Ise järgi" img={building} alt="Building" price="Tasuta"/>
             </DeliveryMethodComponent>
         </div>
     )
--- src/components/CourierOptionComponent/CourierOptionComponent.js
+++ src/components/CourierOptionComponent/CourierOptionComponent.js
@@ -1,12 +1,33 @@
 import "./CourierOptionComponent.css";
 import {Link} from "react-router-dom";
+import React from 'react';
+import {useId} from 'react';
 
+/*
+Courier Component:
+    props:
+    title: Name of delivery service.
+    img: Logo of the delivery service.
+    alt: alt property for the image.
+    price: Price of the delivery service.
+    handleSelected: an object (inherited from parent) that is used to
+        update the button if it's selected and make sure that there is only one courier option selected at a time.
+*/
 
 function CourierOptionComponent(props) {
+    const id = useId();
     return (
-        <Link to={props.link}>
-            <div className="CourierOptionComponent">
+        <Link onClick={() => {
+            if (props.handleSelected.selected === id) {
+                props.handleSelected.useSelected(null);
+            }
+            else {
+                props.handleSelected.useSelected(id);
+            }
 
+        }}>
+            <div className="CourierOptionComponent ">
+                <div className={`SelectedTab ${id === props.handleSelected.selected ? "" : "unselected"}`}/>
                 <img src={props.img} alt={props.alt} className="CourierImg"/>
                 <div className="CourierInfo">
                     <h3 className="CourierName">{props.title}</h3>
