b030a88d517c9f301fbefea8883f97f01711c928
tatall <tatall@taltech.ee>
2022-11-30 23:28:43 +0200

    style fixes

--- src/components/ShoppingCartOverlay/ShoppingCartOverlay.js
+++ src/components/ShoppingCartOverlay/ShoppingCartOverlay.js
@@ -1,39 +1,40 @@
-import classes from "./ShoppingCartOverlay.module.css"
+import classes from "./ShoppingCartOverlay.module.css";
 import ShoppingCartItem from "./ShoppingCartItem";
-import "../../index.css"
-import kjall from "../../assets/kjall.png"
-import bolmen from"../../assets/bolmen.png"
+import "../../index.css";
+import kjall from "../../assets/kjall.png";
+import bolmen from "../../assets/bolmen.png";
 import NavigationButton from "../buttons/NavigationButton/NavigationButton";
 
+export default function ShoppingCartOverlay({ handleShown }) {
+  const isClicked = handleShown.isClicked;
 
-export default function ShoppingCartOverlay({handleShown}){
-   const isClicked = handleShown.isClicked
+  if (!isClicked) return null;
 
-   if (!isClicked) return null
-
-   return (
-      <div className={classes.outerContainer}>
-         <div className={classes.yourCart}>
-            <h2>Sinu ostukorv</h2>
-            <hr className={classes.underLine} />
-         </div>
-         <div>
-            <ShoppingCartItem img={kjall} price="21.99€">Kjallfrogenäider</ShoppingCartItem>
-            <ShoppingCartItem img={bolmen} price="18.99€">Bolmen</ShoppingCartItem>
-         </div>
-         <hr className={classes.underLine} />
-         <div className={classes.total}>
-            Summa: 40.98€
-         </div>
-         <div className={classes.navBtn}>
-            <NavigationButton
-               color="#30CF53"
-               link="/ostukorv"
-               clickFunc={() => handleShown.setClick(false)}>
-               OSTMA
-            </NavigationButton>
-         </div>
+  return (
+    <div className={classes.outerContainer}>
+      <div className={classes.yourCart}>
+        <h2>Sinu ostukorv</h2>
+        <div className={classes.underLine} />
       </div>
-   )
+      <div>
+        <ShoppingCartItem img={kjall} price="21.99€">
+          Kjallfrogenäider
+        </ShoppingCartItem>
+        <ShoppingCartItem img={bolmen} price="18.99€">
+          Bolmen
+        </ShoppingCartItem>
+      </div>
+      <div className={classes.underLine} />
+      <div className={classes.total}>Summa: 40.98€</div>
+      <div className={classes.navBtn}>
+        <NavigationButton
+          color="#30CF53"
+          link="/ostukorv"
+          clickFunc={() => handleShown.setClick(false)}
+        >
+          OSTMA
+        </NavigationButton>
+      </div>
+    </div>
+  );
 }
-
