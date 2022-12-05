d3381260173bc8fe357086713049820a8557b729
Allan Lip <alllip@ttu.ee>
2022-12-05 18:13:20 +0200

    Added text paragraph to blog page.

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -10,27 +10,35 @@ import SocialMediaButton from "../components/buttons/SocialMediaButton/SocialMed
 function BlogPage() {
 
     return (
-        <div className={classes.BlogPage}>
-            <h1 className={classes.title}>
-                Kümme ideed, mida teha kivise ilmaga
-            </h1>
-            <h2 className={`${classes.gray} ${classes.date}`}>Nov 09, 2022</h2>
-            <div className={classes.Socials}>
-                <div className={classes.gray}>jaga</div>
-                <SocialMediaButton img={facebook} alt="facebook"/>
-                <SocialMediaButton img={linkedin} alt="linkedin"/>
-                <SocialMediaButton img={youtube} alt="youtube"/>
-                <SocialMediaButton img={twitter} alt="twitter"/>
-                <SocialMediaButton img={tiktok} alt="tiktok"/>
+        <div className={classes.TopContainer}>
+            <div className={classes.BlogPage}>
+                <h1 className={classes.title}>
+                    Kümme ideed, mida teha kivise ilmaga
+                </h1>
+                <h2 className={`${classes.gray} ${classes.date}`}>Nov 09, 2022</h2>
+                <div className={classes.Socials}>
+                    <div className={classes.gray}>jaga</div>
+                    <SocialMediaButton img={facebook} alt="facebook"/>
+                    <SocialMediaButton img={linkedin} alt="linkedin"/>
+                    <SocialMediaButton img={youtube} alt="youtube"/>
+                    <SocialMediaButton img={twitter} alt="twitter"/>
+                    <SocialMediaButton img={tiktok} alt="tiktok"/>
+                </div>
+                <div className={classes.mainContent}>
+                    <img src={kiviilm} alt="kivine ilm"/>
+                    <p>
+                    Kõiki päevi pole õnnistatud päikesepaiste ja helesinise taevaga. Eestis elades ei tule
+                    tõenäoliselt mitte kellelegi suure üllatusena, et vahel (või tihti) sajab ka kive. Kuigi kivised
+                    päevad võivad tunduda tüütuna, pole vaja oma pead norgu lasta, sest tegelikult pakuvad need ka
+                    palju toredaid võimalusi oma aja sisustamiseks. Sellest blogipostitusest leiad kümme tegevust,
+                    mida ette võtta siis, kui väljas lihtsalt kallab ja kallab.
+                    </p>
+                </div>
             </div>
-            <div className={classes.mainContent}>
-                <img src={kiviilm} alt="kivine ilm"/>
-
-            </div>
-
         </div>
 
 
     )
 }
+
 export default BlogPage;
