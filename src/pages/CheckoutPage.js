bdb442fcb5d5a627fb1752e25488c1d39443d975
tatall <tatall@ttu.ee>
2022-12-08 17:25:59 +0200

    started with checkout

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -5,13 +5,21 @@ import { useSelector } from "react-redux";
 function CheckoutPage() {
   //redux magic. very good explanation -> https://www.youtube.com/watch?v=bbkBuqC1rU4
   const { amount } = useSelector((store) => store.cart);
-  console.log(amount);
+  const paymentHandler = () => {
+    fetch("u4i6z2tgu971d87d65z8a36asmhnj801@hook.eu1.make.com")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="toplevelContainer">
       <div className="leftContainer"></div>
       <div className="rightContainer">
         <CheckoutContainer />
       </div>
+      <button onClick={paymentHandler}>Maksma</button>
     </div>
   );
 }
