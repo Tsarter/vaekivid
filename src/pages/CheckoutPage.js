3c255168b2045caeb69b3c0ca323ed9d13ea66a8
tatall <tatall@ttu.ee>
2022-12-08 23:25:48 +0200

    can buy rock

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,17 +1,48 @@
 import "./CheckoutPage.css";
 import CheckoutContainer from "../components/CheckoutComponent/CheckoutContainer.js";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 function CheckoutPage() {
   //redux magic. very good explanation -> https://www.youtube.com/watch?v=bbkBuqC1rU4
-  const { amount } = useSelector((store) => store.cart);
+  const { amount, cartItems, total } = useSelector((store) => store.cart);
+  const jsonToSend = {};
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(jsonToSend),
+  };
   const paymentHandler = () => {
-    fetch("u4i6z2tgu971d87d65z8a36asmhnj801@hook.eu1.make.com")
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
+    let randomId = Math.floor(Math.random() * 10000000);
+    let payloadObj = {
+      amount: total,
+      currency: "EUR",
+      access_key: "4e5d2fcd-c766-47c4-aa74-6197cf7ddb5e",
+      merchant_reference: toString(randomId),
+      merchant_return_url: "https://tatall.pages.taltech.ee/iti0105-2022/",
+      merchant_notification_url: "https://montonio.com/orders/payment_webhook",
+      payment_information_unstructured:
+        "Payment for order " + toString(randomId),
+      preselected_locale: "et",
+      checkout_email: toString(total) + "@montonio.com",
+    };
+    axios
+      .post(
+        "https://hook.eu1.make.com/j191rhrr099wkqk43jkxom9r3hut8myt",
+        payloadObj
+      )
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        window.open(
+          "https://sandbox-payments.montonio.com?payment_token=" + response.data
+        );
       })
-      .catch((err) => console.log(err));
+      .catch(function (error) {
+        console.log(error);
+      });
   };
   return (
     <div className="toplevelContainer">
