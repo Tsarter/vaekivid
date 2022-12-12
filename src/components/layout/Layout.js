1f936a5add76485a20b0fb7d695bbca4ab8a69d8
Markus <majoas@ttu.ee>
2022-12-12 18:22:41 +0200

    Main Navigation bar is now sticky, changed blogpage css margin top.

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -5,11 +5,9 @@ import FooterComponent from "../FooterComponent/FooterComponent";
 function Layout(props) {
   return (
     <div className={classes.layout}>
-       <div className={classes.nav}>
-         <MainNavigation />
-       </div>
-      <main className={classes.main}>{props.children}</main>
-      <FooterComponent />
+       <MainNavigation />
+       <main className={classes.main}>{props.children}</main>
+       <FooterComponent />
     </div>
   );
 }
