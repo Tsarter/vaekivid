23b93e956a30f210a799b22d54957366d938bb89
Markus <majoas@ttu.ee>
2022-12-14 17:12:42 +0200

    Social media buttons on blog page are clickable.

--- src/components/buttons/SocialMediaButton/SocialMediaButton.js
+++ src/components/buttons/SocialMediaButton/SocialMediaButton.js
@@ -3,8 +3,10 @@ import classes from "./SocialMediaButton.module.css"
 
 export default function SocialMediaButton(props) {
     return (
-        <div className={classes.SocialMediaButton}>
-            <img src={props.img} alt={props.alt}/>
-        </div>
+       <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+           <div className={classes.SocialMediaButton}>
+               <img src={props.img} alt={props.alt}/>
+           </div>
+       </a>
     )
 }
